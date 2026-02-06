923247a636a500b3c475ff7b480c65bfc4f69658
dallaechoi <choi032799@gmail.com>
2026-02-06 13:27:14 +0900

    feat : csrf 검사 off + post create 로직

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('page', index, name='my-page'),
-    path('<int:id>', get_post_detail)
+    #path('', hello_world, name = 'hello_world'),
+    #path('page', index, name='my-page'),
+    #path('<int:id>', get_post_detail)
+
+    path('', post_list, name = "post_list")
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse # 추가
 from django.shortcuts import get_object_or_404 # 추가
 from django.views.decorators.http import require_http_methods
 from .models import *
+import json
 
 # Create your views here.
 
@@ -15,6 +16,45 @@ def hello_world(request):
     
 def index(request):
     return render(request, 'index.html')
+
+# 게시글을 Post(Create), Get(Read) 하는 뷰 로직
+@require_http_methods(["POST"])   #함수 데코레이터, 특정 http method 만 허용합니다
+def post_list(request):
+
+    if request.method == "POST":
+
+        # request.body의 byte -> 문자열 -> python 딕셔너리
+        body = json.loads(request.body.decode('utf-8'))
+
+        # 프론트에게서 user id를 넘겨받는다고 가정.
+		# 외래키 필드의 경우, 객체 자체를 전달해줘야하기 때문에
+        # id를 기반으로 user 객체를 조회해서 가져옵니다 !
+        user_id = body.get('user')
+        user = get_object_or_404(User, pk=user_id)
+
+        # 새로운 데이터를 DB에 생성
+        new_post = Post.objects.create(
+            title = body['title'],
+            content = body['content'],
+            status = body['status'],
+            writer = user
+        )
+
+        # Json 형태 반환 데이터 생성
+        new_post_json = {
+            "id" : new_post.id,
+            "title" : new_post.title,
+            "content" : new_post.content,
+            "status" : new_post.status,
+            "writer" : new_post.writer.id
+        }
+
+        return JsonResponse({
+            'status' : 200,
+            'message' : '게시글 생성 성공',
+            'data' : new_post_json
+        })
+
 
 # Create your views here.
 @require_http_methods(["GET"])

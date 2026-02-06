a49ee676f8873c9bda38dc1a3bfeb0c238cc39df
dallaechoi <choi032799@gmail.com>
2026-02-06 23:32:24 +0900

    fix : get 관련 json 변수이름 통일

--- posts/views.py
+++ posts/views.py
@@ -60,7 +60,7 @@ def post_list(request):
         post_all = Post.objects.all()
 
         # 각 데이터를 Json 형식으로 변환하여 리스트에 저장 (여러개의 게시글 내용을 담을 거라 리스트를 이용합니다)
-        post_json_all = []
+        post_all_json = []
 
         for post in post_all:
             post_json = {
@@ -70,12 +70,12 @@ def post_list(request):
                 "status" : post.status,
                 "writer" : post.writer.username
             }
-            post_json_all.append(post_json)
+            post_all_json.append(post_json)
 
         return JsonResponse({
             'status' : 200,
             'message' : '게시글 목록 조회 성공',
-            'data' : post_json_all
+            'data' : post_all_json
         })
 
 # 게시글 단일조회(GET), 수정(PATCH) 로직
@@ -85,7 +85,7 @@ def post_detail(request, post_id):
     if request.method == "GET":
         post = get_object_or_404(Post, pk=post_id) # post_id 에 해당하는 Post 데이터 가져오기
     
-        post_json_detail = {
+        post_detail_json = {
             "id" : post.id,
             "title" : post.title,
             "content" : post.content,
@@ -95,7 +95,7 @@ def post_detail(request, post_id):
         return JsonResponse({
             "status" : 200,
             'message' : '게시글 단일 조회 성공',
-            "data": post_json_detail})
+            "data": post_detail_json})
     
     if request.method == "PATCH":
         body = json.loads(request.body.decode('utf-8'))
